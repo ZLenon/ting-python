b2382bfe17bf27eda5f11f43b816ea20b292de14
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-07-17 22:52:01 -0300

    Feat: Função txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,28 @@
+# sys é um módulo embutido em Python que fornece acesso recursos relacionados ao sistema.
+# O stderr é um objeto de fluxo de saída de erros.
+import sys
+
+
+# Função para importar um arquivo de texto
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    # Verifica se o caminho do arquivo não possui a extensão .txt
+    # endswith verificar se uma string termina com um determinado sufixo
+    if not str(path_file).endswith(".txt"):
+        # Retorna uma mensagem de formato inválido para a saída de erros padrãos
+        return print("Formato inválido", file=sys.stderr)
+
+    # Lista vazia para armazenar as linhas do arquivo
+    lista = []
+
+    try:
+        # Abre o arquivo no caminho especificado
+        with open(path_file) as file:
+            # Itera sobre as linhas do arquivo
+            for linha in file:
+                # Adiciona a linha à lista de linhas, removendo espaços em branco no início e no fim
+                lista.append(linha.strip())
+        # Retorna a lista de linhas do arquivo
+        return lista
+    except Exception:
+        # Retorna uma mensagem de arquivo não encontrado para a saída de erros padrão
+        return print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -5,7 +5,7 @@ from ting_file_management.abstract_queue import AbstractQueue
 class Queue(AbstractQueue):
     def __init__(self):
         # Inicializa a fila como uma lista vazia
-        self._queue = list()
+        self._fila = list()
         # Inicializa o tamanho da fila como zero
         self._tamanho = 0
 
@@ -15,33 +15,33 @@ class Queue(AbstractQueue):
 
     def __str__(self):
         # Cria uma string vazia para armazenar os elementos da fila
-        str_items = ""
+        elem_fila = ""
         # Itera sobre os elementos na fila
-        for i in range(len(self._queue)):
+        for elem_atual in range(len(self._fila)):
             # Obtém o valor do elemento atual
-            value = self._queue[i]
+            value = self._fila[elem_atual]
             # Converte o valor para uma string e adiciona à string de itens
-            str_items += str(value)
+            elem_fila += str(value)
             # Se não for o último elemento, adiciona uma vírgula e um espaço
-            if i + 1 < len(self._queue):
-                str_items += ", "
+            if elem_atual + 1 < len(self._fila):
+                elem_fila += ", "
 
         # Retorna a representação em string da fila
-        return "Queue(" + str_items + ")"
+        return "Queue(" + elem_fila + ")"
 
     def enqueue(self, value):
         # Adiciona um elemento ao final da fila
-        self._queue.append(value)
+        self._fila.append(value)
         # Incrementa o tamanho da fila
         self._tamanho += 1
 
     def dequeue(self):
         # Remove e retorna o primeiro elemento da fila
-        index = self._queue.pop(0)
+        primeiro_elemento = self._fila.pop(0)
         # Decrementa o tamanho da fila
         self._tamanho -= 1
         # Retorna o elemento removido
-        return index
+        return primeiro_elemento
 
     def search(self, index):
         # Verifica se o índice é inválido ou inexistente
@@ -50,7 +50,7 @@ class Queue(AbstractQueue):
             raise IndexError("Índice Inválido ou Inexistente")
         try:
             # Tenta acessar o elemento no índice fornecido
-            return self._queue[index]
+            return self._fila[index]
         except Exception:
             # Gera uma exceção de índice inválido ou inexistente se ocorrer um erro
             raise IndexError("Índice Inválido ou Inexistente")
