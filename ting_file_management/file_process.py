6f255f1caf22e1bdf4890ca436858c3fbc9a9c09
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-07-18 22:15:00 -0300

    Feat: função remover arquivo file process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,19 @@
-def process(path_file, instance):
+import sys
+
+
+def process(file_path, instancia):
     """Aqui irá sua implementação"""
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instancia):
+    if instancia.__len__() == 0:
+        return print("Não há elementos", file=sys.stdout)
+
+    arquivo = instancia.dequeue()
+    path_file = arquivo["nome_do_arquivo"]
+
+    return print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
 
 
-def file_metadata(instance, position):
+def file_metadata(instancia, posicao):
     """Aqui irá sua implementação"""
