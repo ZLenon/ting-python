88354b03713c10eabfa878ae0decb89a35c1a030
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-07-18 22:44:39 -0300

    Feat: Funçao file Process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,8 +1,28 @@
+from ting_file_management.file_management import txt_importer
 import sys
 
 
 def process(file_path, instancia):
-    """Aqui irá sua implementação"""
+    lista_linhas = txt_importer(file_path)
+    span = {}
+
+    span["nome_do_arquivo"] = file_path
+    span["qtd_linhas"] = len(lista_linhas)
+    span["linhas_do_arquivo"] = lista_linhas
+
+    if instancia.__len__() == 0:
+        instancia.enqueue(span)
+        print(span, file=sys.stdout)
+
+    arquivos = []
+
+    for i in range(0, instancia.__len__()):
+        file = instancia.search(i)
+        arquivos.append(file["nome_do_arquivo"])
+
+    if file_path not in arquivos:
+        instancia.enqueue(span)
+        print(span, file=sys.stdout)
 
 
 def remove(instancia):
@@ -10,9 +30,9 @@ def remove(instancia):
         return print("Não há elementos", file=sys.stdout)
 
     arquivo = instancia.dequeue()
-    path_file = arquivo["nome_do_arquivo"]
+    file_path = arquivo["nome_do_arquivo"]
 
-    return print(f"Arquivo {path_file} removido com sucesso", file=sys.stdout)
+    return print(f"Arquivo {file_path} removido com sucesso", file=sys.stdout)
 
 
 def file_metadata(instancia, posicao):
