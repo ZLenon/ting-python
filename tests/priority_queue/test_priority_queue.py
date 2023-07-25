3ad86445ca6e7d9f8cf8ef9ec4d854b7b111cd85
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-07-25 15:24:23 -0300

    Test: Arquivos de prioridade de fila

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,22 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.file_process import process
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    fila = PriorityQueue()
+    process("statics/novo_paradigma_globalizado.txt", fila)
+    process("statics/nome_pedro.txt", fila)
+    process("statics/novo_paradigma_globalizado-min.txt", fila)
+    process("statics/arquivo_teste.txt", fila)
+    
+    assert len(fila.high_priority) == 2
+    assert len(fila.regular_priority) == 2
+
+    fila.dequeue()
+
+    file_one = fila.search(0)
+    assert file_one["nome_do_arquivo"] == "statics/arquivo_teste.txt"
+
+    with pytest.raises(IndexError):
+        fila.search(10)
