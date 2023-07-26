fc5bf0eb9fe355365c10f66b40c778de3b8b9926
Lenon Nascimento Cardoso <lenoninformatica@gmail.com>
2023-07-26 15:08:39 -0300

    Feat: Req 6 e 7

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,45 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word(mundo, instancia):
+    lista = []
+    dicionario = dict()
+    dicionario["palavra"] = ""
+    dicionario["arquivo"] = ""
+    dicionario["ocorrencias"] = []
+    contador = 1
+
+    for x in range(0, instancia._tamanho):
+        for linha in instancia._fila[x]["linhas_do_arquivo"]:
+            if str(mundo).lower() in str(linha).lower():
+                dicionario["palavra"] = mundo
+                dicionario["arquivo"] = instancia._fila[x]["nome_do_arquivo"]
+                dicionario["ocorrencias"].append({"linha": contador})
+                if len(lista) == 0:
+                    lista.append(dicionario)
+            contador += 1
+        contador = 1
+    return lista
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def search_by_word(mundo, instancia):
+    lista = []
+    dicionario = dict()
+    dicionario["palavra"] = ""
+    dicionario["arquivo"] = ""
+    dicionario["ocorrencias"] = []
+    contador = 1
+
+    for x in range(0, instancia._tamanho):
+        for linha in instancia._fila[x]["linhas_do_arquivo"]:
+            if str(mundo).lower() in str(linha).lower():
+                dicionario["palavra"] = mundo
+                dicionario["arquivo"] = instancia._fila[x]["nome_do_arquivo"]
+                dicionario["ocorrencias"].append(
+                    {
+                        "linha": contador,
+                        "conteudo": linha
+                    }
+                )
+                if len(lista) == 0:
+                    lista.append(dicionario)
+            contador += 1
+        contador = 1
+    return lista
